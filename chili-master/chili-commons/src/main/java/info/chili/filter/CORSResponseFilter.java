ab545d095300a8905f0ee5019b52f4caf0c355e3
Rohith.Vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2017-11-21 08:45:50 -0500

    Updated

--- chili-master/chili-commons/src/main/java/info/chili/filter/CORSResponseFilter.java
+++ chili-master/chili-commons/src/main/java/info/chili/filter/CORSResponseFilter.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 package info.chili.filter;
-
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -14,10 +13,8 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-/**
- *
- * @author Rohith.Vallabhaneni
- */
+
+
 public class CORSResponseFilter implements Filter {
 
     @Override
@@ -27,8 +24,8 @@ public class CORSResponseFilter implements Filter {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, X-XSRF-TOKEN, Content-Type, Content-Length, Accept, X-Requested-With, remember-me");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, remote-ip, X-XSRF-TOKEN, Content-Type, Content-Length, Accept, X-Requested-With, remember-me");
 //        response.setHeader("Access-Control-Expose-Headers", "Origin, X-Requested-With,X-XSRF-TOKEN, Content-Type,Content-Length, Accept, Location");
         chain.doFilter(req, res);
     }
