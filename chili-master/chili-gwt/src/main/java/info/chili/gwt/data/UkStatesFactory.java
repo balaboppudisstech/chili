3412f1557f5b76c5a877d06fd52d947a2a1fb6bc
Rohith.Vallabhaneni <Rohith.Vallabhaneni@SSTechLaptop354.sstech.internal>
2019-02-27 09:27:51 -0500

    Uk States

---
+++ chili-master/chili-gwt/src/main/java/info/chili/gwt/data/UkStatesFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package info.chili.gwt.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Kishore.Chigurupati
+ */
+public class UkStatesFactory {
+
+    public static List<String> getStates() {
+        List<String> states = new ArrayList();
+        states.add("SELECT");
+        states.add("UK");
+        return states;
+    }
+
+}
+
+
+
+
